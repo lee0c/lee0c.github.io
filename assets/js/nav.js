da4b083c197d98dce9bc86613d7aedb23de9caab
Lee Cattarin <lee.cattarin@gmail.com>
2024-09-02 15:24:22 -0700

    flip crow icon with ahndedness toggle

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -56,6 +56,7 @@ function setAlignLeft() {
 	menuList.prepend(themeListItem);
 	menuList.append(alignListItem);
 	menu.after(title);
+	title.classList.add("fa-flip-horizontal");
 }
 
 function setAlignRight() {
@@ -65,6 +66,7 @@ function setAlignRight() {
 	menuList.append(themeListItem);
 	menuList.prepend(alignListItem);
 	menu.before(title);
+	title.classList.remove("fa-flip-horizontal");
 }
 
 function changeAlign(align) {
